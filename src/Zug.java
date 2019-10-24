179b341d58c4172340043c65c23d562622d8e864
janni <janni@DESKTOP-N33I6O0.fritz.box>
2019-10-24 14:43:25 +0200

    First Commit

---
+++ src/Zug.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jannik Feldmann
+ *
+ */
+public class Zug {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
